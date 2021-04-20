7c570f0800d9b3061f8dc48179d84250a2a2c52f
dwisatriow <31796624+dwisatriow@users.noreply.github.com>
2021-04-20 17:25:51 +0000

    Add route handler

--- myApp.js
+++ myApp.js
@@ -1,39 +1,20 @@
 var express = require('express');
 var app = express();
 
+console.log("Hello World")
 
+// app.get("/", function(req, res) {
+//   res.send("Hello Express");
+// });
 
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
+app.get("/json", function(req, res) {
+  res.json({message: "Hello json"});
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.use("/public", express.static(__dirname + "/public"))
 
  module.exports = app;
