2e12005c0aa08a466ba33f218243f7af16e06029
dwisatriow <31796624+dwisatriow@users.noreply.github.com>
2021-05-03 17:18:17 +0700

    Add time server

--- myApp.js
+++ myApp.js
@@ -1,9 +1,9 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
-console.log("Hello World")
+console.log("Hello World");
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var string = req.method + " " + req.path + " - " + req.ip;
   console.log(string);
   next();
@@ -13,18 +13,29 @@ app.use(function(req, res, next) {
 //   res.send("Hello Express");
 // });
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-app.get("/json", function(req, res) {
-  if (process.env['MESSAGE_STYLE'] === 'uppercase') {
-    res.json({message: "Hello json".toUpperCase()});
+app.get("/json", function (req, res) {
+  if (process.env["MESSAGE_STYLE"] === "uppercase") {
+    res.json({ message: "Hello json".toUpperCase() });
   } else {
-    res.json({message: "Hello json"});
+    res.json({ message: "Hello json" });
   }
-})
+});
+
+app.get(
+  "/now",
+  function (req, res, next) {
+    req.time = new Date().toString();
+    next();
+  },
+  function (req, res) {
+    res.json({ time: req.time });
+  }
+);
 
 app.use("/public", express.static(__dirname + "/public"));
 
- module.exports = app;
+module.exports = app;
